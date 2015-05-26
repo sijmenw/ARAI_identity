e4b5282907927f4d2bbff1400668188475cee904
Sijmen van der Willik <sijmenwillik@hotmail.com>
2015-05-26 10:20:03 +0200

    create new index file with form

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// add index with a form to run query with user input
+
+?>
+
+<form action="runQuery.php">
+Query input:<br>
+<input type="text" name="queryInput" value="Frank_van_Harmelen">
+<br><br>
+<input type="submit" value="Submit"></fieldset>
+</form>
